a3e1af257f97d56948bd818809f38c00cc718653
Sid <siddharth.pathre@gmail.com>
2020-01-29 15:06:09 +0530

    gg

---
+++ contactusapicall.js
@@ -0,0 +1,45 @@
+//contact us page message sending api call
+function sendMessage(){
+             document.getElementById("sendmessagebtn").disabled=true;   
+             var firstName =document.getElementById("firstname").value;
+             var Email = document.getElementById("email").value;
+             var mobilenumber = document.getElementById("mobilenumber").value;
+             var message = document.getElementById("message").value;
+            var regUrl = "/institude/support/"
+             if(firstName == "" || Email == "" || message== ""){
+              event.preventDefault();
+              document.getElementById("errorMessage").removeAttribute("hidden");  
+              document.getElementById("sendmessagebtn").disabled=false;
+             }
+             else{
+            document.getElementById("errorMessage").setAttribute("hidden","true");  
+            let payload = {
+                "firstName": firstName,
+                "email": Email,
+                "message": message,
+                "mobile": mobilenumber
+                }
+                console.log("payload",payload);
+                 event.preventDefault();
+                 $.ajax({
+                type: 'POST',
+                url: regUrl,
+                data: JSON.stringify(payload),
+                dataType: 'json',
+                contentType: 'application/json',
+                success: function(data) {
+
+                    if (data && data.status == 200) {
+                       window.location.href="thank-you.html";
+
+                    } else if (data && data.status == 500) {
+                        setAlert(data.message);
+
+                    }
+                },
+                error: function(data) {
+
+                },
+            });
+             }
+        }
